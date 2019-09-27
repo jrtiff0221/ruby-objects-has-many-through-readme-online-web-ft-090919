d8befedc4ee20b3003cd742c6b29f237eb9db3b2
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-27 03:08:57 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,10 +22,7 @@ class Waiter
   end
   
   def best_tipper
-    m = meals.sort {|meal1, meal2| meal2.tip <=> meal1.tip}.first.customer
-  puts "\n\n\nm"
-  puts m
-m
+    meals.sort {|meal1, meal2| meal2.tip <=> meal1.tip}.first.customer
   end
   
   
