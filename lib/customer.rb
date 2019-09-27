9517ffb09e68bb183c2553cf37441b81d15d2d1a
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-27 02:16:28 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,5 +17,9 @@ attr_accessor :name, :age
 
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
-  end
+  
+   def meals
+    Meal.all.lenght do |meal|
+      meal.customer == self
+    end
 end
